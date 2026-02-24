4eb10353882d6c64f0d645850a420a3be5308ad8
Yaqut <yaguter-bpa202@code.edu.az>
2026-02-24 09:32:24 +0400

    Overrided method in IntVector class
    Implemented dynamic resizing for IntVector when adding elements.

--- lab3/src/main/java/org/example/IntVector.java
+++ lab3/src/main/java/org/example/IntVector.java
@@ -1,5 +1,24 @@
 package org.example;
 
 public class IntVector implements IntList{
+ int size=0;
+    public int[] numbers = new int[20];
 
+    @Override
+    public void add(int number) {
+        if (size >= numbers.length) {
+            int newLength = numbers.length*2;
+            int[] newArr = new int[newLength];
+            System.arraycopy(numbers, 0, newArr, 0, numbers.length);
+            numbers = newArr;
+
+        }
+        numbers[size] = number;
+        size++;
+    }
+
+    @Override
+    public int get(int id) {
+        return numbers[i];
+    }
 }
