1b89c0e774de6b19f7af303f476a7b0212d791e0
Yaqut <yaguter-bpa202@code.edu.az>
2026-02-24 09:27:12 +0400

    Edited method

--- lab3/src/main/java/org/example/IntArrayList.java
+++ lab3/src/main/java/org/example/IntArrayList.java
@@ -2,29 +2,25 @@ package org.example;
 
 
 public class IntArrayList implements IntList {
-    int size;
+    int size=0;
     public int[] numbers = new int[10];
 
     @Override
     public void add(int number) {
         if (size >= numbers.length) {
-            int newCapacity = numbers.length + numbers.length / 2;
-            int[] newArr = new int[newCapacity];
+            int newLength = numbers.length + numbers.length / 2;
+            int[] newArr = new int[newLength];
             System.arraycopy(numbers, 0, newArr, 0, numbers.length);
-            newArr[size] = number;
+            numbers = newArr;
 
         }
+        numbers[size] = number;
         size++;
     }
 
     @Override
     public int get(int id) {
-        for (int i = 0; i < numbers.length; i++) {
-            if (id == i) {
-                int result = numbers[id];
-                return result;
-            }
-        }
+        return numbers[i];
     }
 }
 
